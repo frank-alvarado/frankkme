e48c94861fcbbe6f7473e3f173fc028e69b0d1db
Frank Alvarado <franklink1829@gmail.com>
2025-04-22 02:44:42 -0500

    Add Google Analytics tracking with environment variable configuration

--- app/src/pages/_app.jsx
+++ app/src/pages/_app.jsx
@@ -1,9 +1,24 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 import ThemeToggle from '../components/ThemeToggle';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <ThemeToggle />
       <Component {...pageProps} />
     </>
