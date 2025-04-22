ae11f6da129627fa0fc5e466e518ec5c8118b169
Frank Alvarado <franklink1829@gmail.com>
2025-04-22 02:25:10 -0500

    Replace bundle analyzer with Codecov webpack plugin for bundle analysis

--- app/next.config.js
+++ app/next.config.js
@@ -1,20 +1,16 @@
-const path = require('path');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+import { codecovNextJSWebpackPlugin } from "@codecov/nextjs-webpack-plugin";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // enable static HTML export with Next.js 15
-  output: 'export',
-  
-  // Only include standard extensions
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  
-  // Exclude test directories from build output tracing
-  outputFileTracingExcludes: {
-    '*': ['**/__tests__/**/*']
-  }
+export default {
+  webpack: (config, options) => {
+    config.plugins.push(
+      codecovNextJSWebpackPlugin({
+        enableBundleAnalysis: true,
+        bundleName: "frankkme-nextjs-webpack-bundle",
+        uploadToken: process.env.CODECOV_TOKEN,
+        webpack: options.webpack,
+      }),
+    );
+
+    return config;
+  },
 };
-
-module.exports = withBundleAnalyzer(nextConfig);
